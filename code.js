bf5ec55f7918d74a733259a57f8782999ffa20b9
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-03 09:36:31 -0500

    working through

--- code.js
+++ code.js
@@ -3,35 +3,7 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(name){
-    // return 'Say Hello';
-    /* kathleen ver: up to #6
-    * if (name === undefined){
-    *   return "Hello, World";
-    * }
-    * return "Hello, " + name + "!"
-    *>> ALSO ... alternatively for string
-    * >> up to #8
-    * if (typeOf(name) != "string"){
-    *   return "Hello, World";
-    * }
-    * return "Hello, " + name + "!"
-    * }
-    * */
-    var x_name = name;
-    if (x_name === ""){
-        return "";
-    }
-    else if (x_name === 'Jane'){
-        return 'Hello, Jane!'; //ex 3
-    } else if (x_name === 'Ashley') {
-        return 'Hello, Ashley!';// ex 4
-    } else if (x_name === 'Pat'){
-        return 'Hello, Pat!';/* ex 5 check*/
-    } else if(x_name === true){
-        return "Hello, World!"
-    }
-    else {
-        return "Hello, World!"
-    }
+function sayHello(parameter){
+    var name = parameter;
+    return "Hello, " + name + "!";
 }
--- tests.js
+++ tests.js
@@ -16,23 +16,19 @@ describe('helloWorld', function() {
 
 // ****** ****** ****** ADD BY ME ****** ****** ******
 describe('sayHello', function(){
-    it('should be a defined function', function(){
-        expect(typeof sayHello).toBe("function");// ex1
+    it('should be a defined function', function() {
+        expect(typeof sayHello).toBe("function");
     });
-    it('should return a string when called', function() {
-        expect(typeof sayHello()).toBe("string");//ex 2
+    it('should be a string when called', function (){
+        expect(typeof sayHello()).toBe('string');
     });
-    it('should return the string "Hello, Jane!" when executed', function() {
-        expect(sayHello("Jane")).toBe("Hello, Jane!"); //ex 3
+    it('should return "Hello, Jane!" when passed the argument "Jane",', function (){
+        expect(typeof sayHello('Jane')).toBe('string');
     });
-    it('should return the string "Hello, Ashley!" when executed', function() {
-        expect(sayHello('Ashley')).toBe("Hello, Ashley!");//ex 4
+    it('should return "Hello, Alex!" when passed the argument "Alex",', function (){
+        expect(typeof sayHello('Alex')).toBe('string');
     });
-    it('should return the string "Hello, Pat!" when executed', function() {
-        expect(sayHello('Pat')).toBe("Hello, Pat!");// ex5
+    it('should return "Hello, Pat!" when passed the argument "Pat",', function (){
+        expect(typeof sayHello('Pat')).toBe('string');
     });
-    it('should return the string "Hello, World!" when executed', function (){
-        expect(sayHello(undefined)).toBe("Hello, World!");// ex 8
-    });
-
-});
+})
