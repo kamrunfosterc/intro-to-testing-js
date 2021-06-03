37b6df778811ecebe64f1ba23c73fd781a414c4e
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-02 21:23:46 -0500

    add tests and ability to say 'hello' to any input.

--- code.js
+++ code.js
@@ -5,6 +5,19 @@ function helloWorld() {
 
 function sayHello(name){
     // return 'Say Hello';
+    /* kathleen ver: up to #6
+    * if (name === undefined){
+    *   return "Hello, World";
+    * }
+    * return "Hello, " + name + "!"
+    *>> ALSO ... alternatively for string
+    * >> up to #8
+    * if (typeOf(name) != "string"){
+    *   return "Hello, World";
+    * }
+    * return "Hello, " + name + "!"
+    * }
+    * */
     var x_name = name;
     if (x_name === ""){
         return "";
@@ -15,7 +28,10 @@ function sayHello(name){
         return 'Hello, Ashley!';// ex 4
     } else if (x_name === 'Pat'){
         return 'Hello, Pat!';/* ex 5 check*/
-    } else {
+    } else if(x_name === true){
+        return "Hello, World!"
+    }
+    else {
         return "Hello, World!"
     }
 }
